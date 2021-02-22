638e67b2d5e1dc4be3b93974caf65423004c8101
zhourx1129 <534418624@qq.com>
2021-02-22 20:42:09 +0800

    C

--- SelectSort.c
+++ SelectSort.c
@@ -1,7 +1,21 @@
 #include<stdio.h>
 
 int main(){
-    int a[3][4];
-    int sz = sizeof(a[0][0]);
-    printf("%d",sz);
+	int arr[9] = {9,5,3,7,6,8,1,2,4};
+	int i,j,sz;
+	sz = sizeof(arr)/sizeof(arr[0]);
+	for(i=0;i<sz-1;i++){
+		int tmp;
+		for(j=i+1;j<sz;j++){
+			if(arr[j] < arr[i]){
+				tmp = arr[j];
+				arr[j] = arr[i];
+				arr[i] = tmp;
+			}
+		}
+	}
+	for(i=0;i<sz;i++)
+		printf("%d",arr[i]);
+	printf("\n");
+	return 0;
 }
