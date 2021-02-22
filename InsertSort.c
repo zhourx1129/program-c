da5ca513de69dc7936c3ce70ac9b57a525f6d125
zhourx1129 <534418624@qq.com>
2021-02-22 22:05:36 +0800

    c

---
+++ InsertSort.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int arr[9] = {9,5,3,7,6,8,1,2,4};
+	int i,j,tmp,sz;
+	sz = sizeof(arr)/sizeof(arr[0]);
+	for (i=1;i<sz;i++){
+		for (j=i;j>0;j--){
+			if(arr[j]<arr[j-1]){
+				tmp = arr[j-1];
+				arr[j-1] = arr[j];
+				arr[j] = tmp;
+			}
+		}
+	}
+	for (i=0;i<sz;i++)
+		printf("%d",arr[i]);
+	printf("\n");
+}
