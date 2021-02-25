9ded20b2a3bbac920979f6dc99ea47a1696b10ac
zhourx1129 <534418624@qq.com>
2021-02-25 20:25:18 +0800

    死循环

--- Test.c
+++ Test.c
@@ -1,11 +1,15 @@
 #include<stdio.h>
 
-int main(){
+int main()
+{
+	int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+	int j;
 	int i=0;
-	int arr[10] = {1,2,3,4,5,6,7,8,9};
+	// printf("&i=%d &arr=%d &j=%d\n",&i,arr,&j);
 	for(i=0;i<=11;i++){
 		printf("hehe\n");
 		arr[i]=0;
 	}
+	// printf("%d",&arr[10]);
 	return 0;
 }
