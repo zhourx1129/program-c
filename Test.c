f99a8ff1d7ad01ee0e7cc668a00141d12d1931c7
zhourx1129 <534418624@qq.com>
2021-02-26 10:22:02 +0800

    测试死循环

--- Test.c
+++ Test.c
@@ -2,14 +2,23 @@
 
 int main()
 {
+	// 死循环    数组下标越界误认为是i
+	/*
 	int arr[10] = {1,2,3,4,5,6,7,8,9,10};
-	int j;
 	int i=0;
-	// printf("&i=%d &arr=%d &j=%d\n",&i,arr,&j);
+	printf("&i=%d &arr=%d &j=%d\n",&i,arr,&j);
 	for(i=0;i<=11;i++){
 		printf("hehe\n");
 		arr[i]=0;
 	}
-	// printf("%d",&arr[10]);
+	*/
+
+	i是无符号数,无符号数最小也是0 所以一直满足i>=0,即为死循环
+	/*
+	unsigned int i;
+	for(i=9;i>=0;i--){
+		printf("%u\n",i);
+	}
+	*?
 	return 0;
 }
